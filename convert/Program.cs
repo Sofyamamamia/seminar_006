3547bb18a128075b2d9ed023126f1f62f2a92c39
Sofya <vanieva.sofya@mail.ru>
2023-08-22 12:43:38 +0300

    deal

---
+++ convert/Program.cs
@@ -0,0 +1,23 @@
+﻿{
+    static void Main(string[] args)
+    {
+        int decimalNumber = 46;
+        Console.WriteLine($"Десятичное число: {decimalNumber}");
+
+        string binaryNumber = DecimalToBinary(decimalNumber);
+        Console.WriteLine($"Двоичное число: {binaryNumber}");
+    }
+
+    static string DecimalToBinary(int decimalNumber)
+    {
+        string binaryNumber = "";
+        while (decimalNumber > 0)
+        {
+            int remainder = decimalNumber % 2;
+            binaryNumber = remainder + binaryNumber;
+            decimalNumber /= 2;
+        }
+        return binaryNumber;
+    }
+}
+
