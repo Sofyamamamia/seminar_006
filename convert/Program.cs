f0046b771610abd054bc39b062e841c90a751bd0
Sofya <vanieva.sofya@mail.ru>
2023-08-25 00:11:52 +0300

    well...

--- convert/Program.cs
+++ convert/Program.cs
@@ -1,19 +1,17 @@
-﻿using System;
+﻿
 
-class Program {
-    static void Main(string[] args) {
-        Console.Write("Введите количество чисел: ");
-        int m = int.Parse(Console.ReadLine());
-        int count = 0;
+Console.Write("Введите количество чисел: ");
+int m = int.Parse(Console.ReadLine());
+int count = 0;
 
-        for (int i = 0; i < m; i++) {
-            Console.Write($"Введите число {i + 1}: ");
-            int num = int.Parse(Console.ReadLine());
-            if (num > 0) {
-                count++;
-            }
-        }
-
-        Console.WriteLine($"Количество чисел больше 0: {count}");
+for (int i = 0; i < m; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int num = int.Parse(Console.ReadLine());
+    if (num > 0)
+    {
+        count++;
     }
 }
+
+Console.WriteLine($"Количество чисел больше 0: {count}");
